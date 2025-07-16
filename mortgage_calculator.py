80b36ae153d02a92e5612bb93cfbc7bbf0d4dd89
Chiamie <okechukwurchiamaka@gmail.com>
2025-07-16 15:43:02 +0100

    Add Class_work_python

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -2,8 +2,11 @@ principal = int(input("Enter the amount you wish to borrow: "))
 annualRate = float(input("Enter the yearly interest on the loan: "))
 loanDuration_Years = int(input("Enter the duraton of the loan in years: "))
 
-monthlyRate = annualRate / (100 * 12)
-loanDuration_Months = loanDuration_Years * 12
+PERCENTAGE = 100
+MONTHS_IN_YEAR = 12
+
+monthlyRate = annualRate / (PERCENTAGE * MONTHS_IN_YEAR)
+loanDuration_Months = loanDuration_Years * MONTHS_IN_YEAR
 
 monthlyPayment_Value = principal * ((monthlyRate * ((1 + monthlyRate) ** loanDuration_Months)) / (((1 + monthlyRate) ** loanDuration_Months) - 1))
 print("Your monthly payment value is ", monthlyPayment_Value )
