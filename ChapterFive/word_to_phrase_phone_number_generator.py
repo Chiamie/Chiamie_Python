7a591ca44ba142b2e0d6f05a28d357e5a1f8b8e6
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-20 18:40:18 +0100

    added generate vanity phone numbers function

---
+++ ChapterFive/word_to_phrase_phone_number_generator.py
@@ -0,0 +1,23 @@
+#5.13 (Word or Phrase to Phone-Number Generator)
+
+
+
+def generate_vanity_phone_numbers(string):
+	digit_to_letters = [("2", "ABC"), ("3", "DEF"), ("4", "GHI"), ("5", "JKL"), ("6", "MNO"), ("7", "PRS"), ("8", "TUV"), ("9", "WXY")]
+	
+	combinations = [""]
+	for character in string:
+		new_combination = []
+		for combination in combinations:
+			for element in digit_to_letters:
+				for letter in element[1]:
+					if character == letter:
+						new_combination.append(combination + element[0])
+		combinations = new_combination
+	return combination
+				
+
+
+
+word = "PETCARE"
+print(generate_vanity_phone_numbers(word))
