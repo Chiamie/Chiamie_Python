9bca86dbcacc06d606418eca47b178f35657b890
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-20 13:13:39 +0100

    yet to simulate the function using stack

---
+++ ChapterFive/palindrome_tester.py
@@ -0,0 +1,30 @@
+#5.9 (Palindrome Tester) 
+def is_palindrome(string):
+	return string[::-1] == string
+			
+machine = "radar"
+
+print(is_palindrome(machine))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
