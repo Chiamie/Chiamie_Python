4a4ff7a07f98a5866273e76cb146ccb091a0574a
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-20 12:14:32 +0100

    improved the performance of the algorithm by seting to false only multiples of 2, 3, 5, 7, 9, 11,

--- ChapterFive/sieve_of_eratosphenes.py
+++ ChapterFive/sieve_of_eratosphenes.py
@@ -5,14 +5,14 @@ list_primes = [True] * 1000
 
 count = 2
 for index in range(2, 1000):
-	if list_primes[index] == True:
-		for count in range(index + 1, 1000):
-			if count % index == 0:
-				list_primes[count] = False
+	if index in [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31]:
+		if list_primes[index] == True:
+			for count in range(index + 1, 1000):
+				if count % index == 0:
+					list_primes[count] = False
 	
 
 print(list_primes)
-
 
 
 
