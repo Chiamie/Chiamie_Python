3e6ee65e4e3077b4995b3c8f04535fc174483f36
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-21 15:11:23 +0100

    added map the list of even numbers by tripling each of them and get the total of the triple of even numbers from 1 through 10

--- ChapterFive/summing_the_triples_of_the_even_integers_from_2_through_10.py
+++ ChapterFive/summing_the_triples_of_the_even_integers_from_2_through_10.py
@@ -16,6 +16,22 @@ def get_even_numbers_of(x):
 list_of_even_integers = list(filter(get_even_numbers_of, list_of_integers))
 
 
+#map the list of even numbers ny tripling each of them
+def triple_each_element_of(x):
+	return x ** 3
+	
+list_of_tripled_integers = list(map(triple_each_element_of, list_of_even_integers))
+
+#get the total of the triple of even numbers from 1 through 10
+
+total_of_the_triples_of_even_numbers = sum(list_of_tripled_integers)
+print("The total of the triples of the even integers from 1 through 10 is " + str(total_of_the_triples_of_even_numbers))
+
+
+#Reimplementing the above code with list comprehension
+
+#using a list comprehension to create a list of integers
+
 		
 
 
