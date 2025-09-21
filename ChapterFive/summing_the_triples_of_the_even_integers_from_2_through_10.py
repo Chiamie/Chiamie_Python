c10ee269cddf8f91c10f343f219add51183e187f
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-21 14:58:42 +0100

    added creating_a_list function

---
+++ ChapterFive/summing_the_triples_of_the_even_integers_from_2_through_10.py
@@ -0,0 +1,32 @@
+#5.18 (Summing the Triples of the Even Integers from 2 through 10)
+
+def creating_a_list():
+	list_of_integers = []
+	for number in range(1, 11):
+		list_of_integers.append(number)
+	return list_of_integers
+	
+
+
+
+
+	
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
