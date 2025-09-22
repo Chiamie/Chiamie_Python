fd1a2b61dcfb710adb1d70c4475b1d30116528c7
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-22 04:05:58 +0100

    added counting duplicate words, the script ignores case

---
+++ ChapterSix/counting_duplicate_words.py
@@ -0,0 +1,19 @@
+#6.5 (Counting Duplicate Words)
+
+
+text = ('God is not man, man is not God but man is a god.')
+word_counts = {}
+for word in text.lower().split():
+	if word in word_counts:
+		word_counts[word] += 1  
+	else:
+ 		word_counts[word] = 1  
+print(f'{"WORD":<12}COUNT')
+for word, count in sorted(word_counts.items()):
+	print(f'{word:<12}{count}')
+print('\nNumber of unique words:', len(word_counts))
+
+
+
+
+
