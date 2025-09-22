9a284b3be313d763d52153f36c2a2d9bc7117aad
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-22 05:14:09 +0100

    refactored the output to be in a tabular form with the letters in alpabetical order

--- ChapterSix/character_counts.py
+++ ChapterSix/character_counts.py
@@ -7,7 +7,7 @@ modified_user_input = re.sub(r'[^\w\s]', '', user_input)
 
 modified_user_input = modified_user_input.lower().split()
 
-print(modified_user_input)
+
 letter_counts = {}
 for word in modified_user_input:
 	word.split()
@@ -17,7 +17,10 @@ for word in modified_user_input:
 		else:
 			letter_counts[letter] = 1
 
-print(letter_counts)
+print("LETTER\tCOUNT")
+for key, value in sorted(letter_counts.items()):
+	print(f'{key}:\t{value}')
+
 
 
 
