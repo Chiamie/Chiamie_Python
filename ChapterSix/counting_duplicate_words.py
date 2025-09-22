10500ea131cc42f0ba2afec2907ef6994f9a221d
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-22 04:12:04 +0100

    refactored the script to assume no punctuation

--- ChapterSix/counting_duplicate_words.py
+++ ChapterSix/counting_duplicate_words.py
@@ -1,9 +1,9 @@
 #6.5 (Counting Duplicate Words)
 
-
 text = ('God is not man, man is not God but man is a god.')
+text_with_no_punctuation = re.sub(r'[^\w\s]', '', text)
 word_counts = {}
-for word in text.lower().split():
+for word in text_with_no_punctuation.lower().split():
 	if word in word_counts:
 		word_counts[word] += 1  
 	else:
