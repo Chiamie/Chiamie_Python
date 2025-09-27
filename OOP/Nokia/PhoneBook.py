4951e961b802ff24684cb13218a2e0d627b945d9
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-27 15:33:47 +0100

    added test_that_contact_can_be_added_and_gotten_back and test_that_contacts_book_values_can_be_edited tests

--- OOP/Nokia/PhoneBook.py
+++ OOP/Nokia/PhoneBook.py
@@ -1,17 +1,31 @@
-class Tone:
-    pass
-
 
 class Phonebook:
     def __init__(self):
         self.contacts_book = {}
-        self.name = ""
-        self.number = ""
-        self.tone = Tone()
 
     def is_empty(self):
-        return True
+        return self.contacts_book == {}
+
+    def add_contact(self, contact):
+        self.contacts_book[contact[0]] = contact[1]
+
+
+    def edit_contact(self, name, new_contact):
+        if not name.isalpha():
+            raise ValueError('Name must be an apha!')
+        if not new_contact.isdigit():
+            raise ValueError('New contact must be an integer!')
+        for contact in self.contacts_book:
+            if contact == name and new_contact.isdigit():
+                self.contacts_book[name] = new_contact
+            elif contact == name and new_contact.isalpha():
+                self.contacts_book[new_contact] = self.contacts_book[name].pop(name)
+
 
     def search(self, name):
-        return self.contacts_book[name]
+        return self.contacts_book.get(name)
 
+    def get_contact(self):
+        return self.contacts_book
+
+    def erase(self):
--- OOP/Nokia/test_phone_book.py
+++ OOP/Nokia/test_phone_book.py
@@ -7,8 +7,28 @@ class TestPhoneBook(unittest.TestCase):
     def setUp(self):
         self.phone_book = Phonebook()
 
-    def test_that_phonebook_is_empty(self):
-        self.assertTrue(self.phone_book.is_empty(), False)  # add assertion here
+    def test_that_contacts_book_is_empty(self):
+        self.assertTrue(self.phone_book.is_empty(), True)
+
+    def test_that_contacts_book_is_not_empty_when_item_is_added_to_it(self):
+        contact = ["chiamaka", "08101235568"]
+        self.phone_book.add_contact(contact)
+        self.assertFalse(self.phone_book.is_empty(), False)# add assertion here
+
+    def test_that_contact_can_be_added_and_gotten_back(self):
+        contact = ["chiamaka", "08101235568"]
+        self.phone_book.add_contact(contact)
+
+        #result = self.phone_book.search("chiamaka")
+        self.assertEqual(self.phone_book.get_contact(), {"chiamaka": "08101235568"})
+
+    def test_that_contacts_book_values_can_be_edited(self):
+        contact = ["chiamaka", "08101235568"]
+        self.phone_book.add_contact(contact)
+
+
+        self.phone_book.edit_contact("chiamaka", "08101111111")
+        self.assertEqual(self.phone_book.search("chiamaka"),  "08101111111")
 
 
 if __name__ == '__main__':
