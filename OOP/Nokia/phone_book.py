40591156495dd2213497a1317c43055d1ab79edb
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-27 12:29:16 +0100

    started

---
+++ OOP/Nokia/phone_book.py
@@ -0,0 +1,11 @@
+class Tone:
+    pass
+
+
+class Phonebook:
+    def __init__(self):
+        self.contact = []
+        self.name = ""
+        self.number = ""
+        self.tone = Tone()
+        
