9e5904138ce50717bceb189b16fbb5962292c6d6
Chiamie <okechukwurchiamaka@gmail.com>
2025-10-10 02:16:48 +0100

    ArrayList now have the following behaviours: contains, index_of, last_index_of

--- python assignment/DSA/ArrayList.py
+++ python assignment/DSA/ArrayList.py
@@ -1,22 +1,97 @@
+import collections
+
 from DSA.Array import Array
 
 
 class ArrayList:
-    array_list = []
     count = 0
     size = 0
-    def __init__(self, capacity=10, collections=None):
+
+    def __init__(self, data_type, capacity=10, values=None):
+        self.array_list = [data_type()]
+        self.data_type = data_type
         self.size = capacity
-        if collections is not None:
-           self.array_list = Array(len(collections), object)
-           self.array_list.data[:] = collections
-           self.count = len(collections)
+        if values is not None:
+           self.array_list = Array(len(values), data_type)
+           self.array_list.data[:] = values
+           print(self.array_list)
+           self.count = len(values)
         else:
-            self.array_list = Array(capacity, object)
+            self.array_list = Array(capacity, data_type)
             self.count = 0
 
     def is_empty(self):
         return self.count == 0
+
+    def __resize(self, new_capacity):
+        new_array = Array(new_capacity, self.data_type)
+        for index in range(self.count):
+            new_array.data[index] = self.array_list.data[index]
+        self.array_list = new_array
+        self.array_list.size = new_capacity
+
+    def add(self, value):
+        if self.count == self.size:
+            self.__resize(self.size * 2)
+        self.array_list[self.count] = value
+        self.count += 1
+
+    def get_array_list(self):
+        return self.array_list
+
+    def contains(self, value):
+        for index in range(0, self.size):
+            if self.array_list[index] == value:
+                return True
+        return False
+
+    def index_of(self, value):
+        for index in range(0, self.size):
+            if self.array_list[index] == value:
+                return index
+        return -1
+
+    def last_index_of(self, value):
+        index = -1
+        for counter in range(0, self.size):
+            if self.array_list[counter] == value:
+                index = counter
+        return index
+
+    def
+    get(int
+    index){
+    if (index < 0 | | index >= array.length)
+    throw
+    new
+    IndexOutOfBoundsException("Index is out of bounds!");
+    return array[index];
+    }
+
+    public
+    Object
+    set(int
+    index, Object
+    value){
+    if (index < 0 | | index >= array.length)
+        throw
+        new
+        IndexOutOfBoundsException("Index is out of bounds!");
+
+    Object
+    oldValue = array[index];
+    array[index] = value;
+    return oldValue;
+
+}
+
+def __str__(self):
+        return str(self.array_list)
+
+    def __repr__(self):
+        print(self.array_list)
+
+
 
 
 
--- python assignment/DSA/TestArrayList.py
+++ python assignment/DSA/TestArrayList.py
@@ -5,26 +5,81 @@ from DSA.ArrayList import ArrayList
 
 class TestArrayList(unittest.TestCase):
     def setUp(self):
-        self.array_list = ArrayList(10)
+        self.array_list = ArrayList(int,10)
 
     def test_that_arrayList_is_empty(self):
         self.assertTrue(self.array_list.is_empty())
 
     def test_that_arrayList_can_be_constructed_using_collections(self):
         ages = [34, 23, 12, 45, 2, 37]
-        self.my_array_list = ArrayList(collections=ages)
+        self.my_array_list = ArrayList(int, values=ages)
         self.assertFalse(self.my_array_list.is_empty())
 
     def test_that_arrayList_can_be_constructed_using_empty_constructor_and_will_have_10_capacity(self):
-        self.my_array_list = ArrayList()
-        self.assertEqual(self.my_array_list.size, 10)
-        self.assertTrue(self.my_array_list.is_empty())
+        my_array_list = ArrayList(int)
+        self.assertEqual(my_array_list.size, 10)
+        self.assertTrue(my_array_list.is_empty())
 
     def test_that_arrayList_can_be_constructed_using_empty_constructor_and_will_be_empty(self):
-        self.my_array_list = ArrayList()
-        self.assertTrue(self.my_array_list.is_empty())
+        my_array_list = ArrayList(int)
+        self.assertTrue(my_array_list.is_empty())
+
+    def test_that_the_capacity_of_arrayList_increases_when_array_is_full(self):
+        my_array_list = ArrayList(int,3)
+        my_array_list.add(23)
+        my_array_list.add(62)
+        my_array_list.add(28)
+        my_array_list.add(41)
+
+        print(my_array_list)
+        print(my_array_list.get_array_list())
+
+        self.assertEqual(my_array_list.get_array_list(),  [23, 62, 28, 41, 0, 0])
+
+    def test_that_arrayList_contains_the_specified_element(self):
+        self.array_list.add(12)
+        self.array_list.add(32)
+        self.array_list.add(21)
+
+        self.assertTrue(self.array_list.contains(12))
+
+    def test_that_arrayList_does_not_contain_the_specified_element(self):
+        self.array_list.add(12)
+        self.array_list.add(32)
+        self.array_list.add(21)
+
+        self.assertFalse(self.array_list.contains(42))
+
+    def test_that_arrayList_returns_the_index_of_the_first_occurrence_of_the_specified_element(self):
+        self.array_list.add(12)
+        self.array_list.add(32)
+        self.array_list.add(21)
+        self.array_list.add(41)
+
+        self.assertEqual(self.array_list.index_of(32), 1)
 
 
+    def test_that_arrayList_returns_the_index_of_the_last_occurrence_of_the_specified_element(self):
+        self.array_list.add(12)
+        self.array_list.add(32)
+        self.array_list.add(21)
+        self.array_list.add(41)
+        self.array_list.add(12)
+        self.array_list.add(53)
 
+        self.assertEqual(self.array_list.last_index_of(12), 4)
+
+
+    def test_that_arrayList_returns_minus_one_if_the_specified_element_is_not_in_the_arraylist(self):
+        self.array_list.add(12)
+        self.array_list.add(32)
+        self.array_list.add(21)
+        self.array_list.add(41)
+        self.array_list.add(12)
+        self.array_list.add(53)
+
+        self.assertEqual(self.array_list.last_index_of(8), -1)
+
+    def test_that_arraylist_
 if __name__ == '__main__':
     unittest.main()
