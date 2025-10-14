968319778e8c481298236f0e030c8c8293daa48c
Chiamie <okechukwurchiamaka@gmail.com>
2025-10-14 12:46:53 +0100

    added get_all_palindromic_strings

--- Self/Longest_Palindromic_Substring.py
+++ Self/Longest_Palindromic_Substring.py
@@ -6,3 +6,11 @@ def get_all_sub_string(group_of_characters):
 			sub_string += group_of_characters[counter]
 			list_of_sub_string.append(sub_string)
 	return list_of_sub_string	
+
+
+def get_all_palindromic_strings(list_of_sub_string):
+	list_of_palindromic_strings = []
+	for element in list_of_sub_string:
+		if element[::-1] == element:
+			list_of_palindromic_strings.append(element)
+	return list_of_palindromic_strings
